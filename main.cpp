5dff44eb93d3b4ddc4fc3bd079f49008fe01d402
Dustin Kent Greco <dkgreco@thetroveofgems.tech>
2021-12-20 16:25:10 -0700

    Chapter 2 - Base Work

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
+/* File Corresponds To Chapter 2 - Jumping Into C++ Alex Allain ISB:978-0-9889278-0-3
+ *
+ * */
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+#include <iostream> // PURPOSE: Imports iostream library
+
+using namespace std; // PURPOSE: Shortens the named exports from the iostream file for easier invocation.
+
+int main() { // PURPOSE: C++ Main Function Returns Integer, Takes No Arguments. Runs the code in the body.
+    cout << "Hey You! I'm Alive! A Porygon Hello World!" << endl;
+    // cout and endl shorthand due to namespace std.
+    // String output from << modifiers
+
+
+    return 0; // PURPOSE: 0 => Program Success || 1 => Program Failure
 }
