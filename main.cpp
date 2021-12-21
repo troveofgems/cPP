ff410af7e244453ed05a4165df989776e47e0fe6
Dustin Kent Greco <dkgreco@thetroveofgems.tech>
2021-12-21 15:26:16 -0700

    Prepping Space for Chapter 3 Work

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 /* File Corresponds To Chapter 2 - Jumping Into C++ Alex Allain ISB:978-0-9889278-0-3
  *  Practice Problem 2
- *  Write a program that prints the names of your friends onto their own lines
- *  using only a single cout and two strings.
+ *  Write a program that prints the names of your friends onto their own lines using only a single cout and two strings.
  * */
 
 #include <iostream>
